b23127098d902f6b5f7a64900e9b024a6c117cd2
Tamerlan Egorov <egtamerlan@gmail.com>
2025-07-03 10:42:10 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,16 +1,11 @@
 def copy_file(command: str) -> None:
     try:
         commands = command.split()
-        command_cd = commands[0]
-        file_txt = commands[1]
-        new_file = commands[2]
+        command_cd, file_txt, new_file = commands[0], commands[1], commands[2]
     except IndexError:
         return
 
-    if file_txt == new_file:
-        return
-
-    if command_cd != "cp":
+    if file_txt == new_file or command_cd != "cp":
         return
 
     try:
